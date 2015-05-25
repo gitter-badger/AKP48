ccad0ad387e464219ce036bc09cb216cba5d6ea4
Austin Peterson <austin@akpwebdesign.com>
2015-05-24 19:24:39 -0500

    We check for permissions inside of commands only now.
    It's up to the command to decide whether or not to respond to a user.

--- Commands/nick.js
+++ Commands/nick.js
@@ -28,9 +28,6 @@ function Nick() {
     //ways to call this command.
     this.aliases = ['nick'];
 
-    //The power level needed to use this command.
-    this.powerLevel = 9000;
-
     //whether or not to allow this command in a private message.
     this.allowPm = true;
 
@@ -39,6 +36,10 @@ function Nick() {
 }
 
 Nick.prototype.execute = function(context) {
+    if(config.getPerms().powerLevelFromContext(context) < config.powerLevels[context.getClient().uuid]["root"]) {
+        return true;
+    }
+
     if(context.arguments[0]) {
         context.client.getIRCClient().send("NICK", context.arguments[0]);
         context.client.nick = context.arguments[0];
